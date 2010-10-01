685cc7e1f66a912d8b6ac23972b3a01318f6b2e8
Jon Parise <jon@booyah.com>
2010-09-30 19:53:54 -0700

    Adding some forward type declarations.
    This file no longer needs to rely on ProtocolBuffers.h or
    ForwardDeclarations.h to be included first in order for it to compile.

--- src/runtime/Classes/Message.h
+++ src/runtime/Classes/Message.h
@@ -15,6 +15,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+@class PBCodedOutputStream;
+@class PBUnknownFieldSet;
+@protocol PBMessage_Builder;
+
 /**
  * Abstract interface implemented by Protocol Message objects.
  *
