19d5f017264ecb7c167dffb17b38779dbeb1bdcb
Jon Parise <jon@booyah.com>
2010-10-13 09:52:15 -0700

    The m4 directory must exist before autoreconf runs.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,7 @@
 # files aren't managed by source control but are included in the distribution.
 
 set -e
+mkdir -p m4
 autoreconf -f -i -Wall
 rm -rf autom4te.cache configure.ac~ config.h.in~
 exit 0
